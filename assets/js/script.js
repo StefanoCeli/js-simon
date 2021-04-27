ef13a0c948e638e431d358ba0cfd50034b6edd1a
StefanoCeli <stefanopsn91@gmail.com>
2021-04-27 02:49:40 +0200

    inserito commenti

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,22 @@
 $(document).ready(function(){
+
     //finchè non viene letto il document tutto il container è display none
     $('.container').show();
+
     //creo gli array vuoti
     var arrNumRandom = [];
     var arrNumUser = [];
     var arrNumResult = [];
+
     //chiedo all'utente con quanti numeri vuole giocare
-    var limitUser = parseInt(prompt('Con quanti numeri vuoi giocare?'))
+    var limitUser = parseInt(prompt('Con quanti numeri vuoi giocare?'));
+
     //setto la situazione di default
     reset();
+
     //tramite il click su start faccio scomparire il bottone e genero i numeri random
     $('#start').click(function(){
+
         $(this).hide();
         while(arrNumRandom.length < limitUser){
             arrNumRandom.push(getRandomNumber(1,100)); 
@@ -23,14 +29,17 @@ $(document).ready(function(){
             printOutput('inserisci i numeri','#output')
         },5000)
     });
+
     //al click di invia vado a memorizzare in un array i numeri inseriti dall'utente
     $('#invia').click(function(){
 
         if(arrNumUser.length < limitUser){
+
                 arrNumUser.push($('#box input').val());
                 $('#box input').val('');
                 //al raggiungimento del limite inserito dall'utente faccio sparire di nuovo il box e faccio apparire "cacolo in corso"
                 if(arrNumUser.length ===limitUser){
+
                     $('#box').hide();
                     printOutput('Calcolo in corso','#output');
                     //setto un timeout di 3 secondi
@@ -39,12 +48,14 @@ $(document).ready(function(){
                         var i = 0;
                         //inizio un ciclo while sull'array dell'utente
                         while( i < arrNumUser.length){
+
                             //controllo se l'array random contiene i numeri dell'utente,se il risultato è positivo allora pusherà nell'array vuoto
                             if(arrNumRandom.includes(parseInt(arrNumUser[i]))){
                                 arrNumResult.push(parseInt(arrNumUser[i]));
                             }
                             i++;
                         }
+
                         //se la lunghezza dell'array result è uguale a quello random l'utente ha vinto
                         if(arrNumResult.length === arrNumRandom.length){
                             printOutput('Complimenti! hai indovinato tutti i numeri! i numeri erano :' + arrNumResult ,'#output')
@@ -56,11 +67,13 @@ $(document).ready(function(){
                             printOutput('Hai indovinato ' + arrNumResult.length + ' numeri su ' + arrNumRandom.length + ' i numeri sono: ' + arrNumResult,'#output');
                             
                         }
+
                         //al termine del gioco faccio apparire il pulsante restart,al click ricaricherà la pagina
                         $('#restart').show();
                         $('#restart').click(function(){
                             location.reload();
                         });
+
                     },3000);
                 }
         }
@@ -68,6 +81,7 @@ $(document).ready(function(){
     });
 
 });
+
 //funzione di reset
 function reset(){
     printOutput('Clicca via!','#output')
@@ -80,6 +94,7 @@ function reset(){
 function printOutput(str,target){
 $(target).text(str);
 };
+
 //funzione numeri random
 function getRandomNumber(min, max){
     return Math.floor(Math.random()*(max - min + 1)+min);
